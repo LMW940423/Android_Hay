d666c3f8a1175b667639effb9082037a362657ac
chlgusdk28 <hyeona_c@naver.com>
2020-12-28 22:56:11 +0900

    intro complete

--- Hay/app/src/main/java/com/android/mypeople/IntroActivity.java
+++ Hay/app/src/main/java/com/android/mypeople/IntroActivity.java
@@ -2,13 +2,34 @@ package com.android.mypeople;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
 
-public class IntroActivity extends AppCompatActivity {
 
+public class IntroActivity extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_intro);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(IntroActivity.this,LoginActivity.class);
+                IntroActivity.this.startActivity(mainIntent);
+                IntroActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
