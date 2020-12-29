6d1183bbfd2f38fbed5233613313bc852d5e5e3c
yonsse <3318yonsse@naver.com>
2020-12-29 14:31:46 +0900

    Update LoginActivity.java

--- Hay/app/src/main/java/com/android/mypeople/LoginActivity.java
+++ Hay/app/src/main/java/com/android/mypeople/LoginActivity.java
@@ -12,6 +12,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.sql.Timestamp;
@@ -140,6 +141,11 @@ public class LoginActivity extends AppCompatActivity {
 
                     }else{
                         Toast.makeText(LoginActivity.this, "회원정보를 확인해주세요", Toast.LENGTH_SHORT).show();
+                        new AlertDialog.Builder(LoginActivity.this)
+                                .setTitle("입력 정보 오류")
+                                .setMessage("입력정보를 확인하세요")
+                                .setPositiveButton("확인",null)
+                                .show();
                     }
                     break;
 
