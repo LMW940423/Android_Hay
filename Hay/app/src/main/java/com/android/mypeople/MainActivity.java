4ebbc4d8dad01253621bffd0fff6bc5f4c419bac
yonsse <3318yonsse@naver.com>
2020-12-29 22:49:17 +0900

    Update MainActivity.java
    MainActivity.java // Import

--- Hay/app/src/main/java/com/android/mypeople/MainActivity.java
+++ Hay/app/src/main/java/com/android/mypeople/MainActivity.java
@@ -8,9 +8,11 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
